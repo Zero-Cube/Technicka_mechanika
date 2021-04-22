5808fbfe6700a1582c2c7533463bf74439fbdbb0
palino2pp <82977206+palino2pp@users.noreply.github.com>
2021-04-22 20:57:08 +0200

    Update Výpočet tepelnej rozťaznosti.py

--- Výpočet tepelnej rozťaznosti.py
+++ Výpočet tepelnej rozťaznosti.py
@@ -1,28 +1,30 @@
 ### Program pri neznámej a
+a = False
+ΔL = True
+if a == False:
+    print("PROGRAM PRI NEZNAMEJ a")
 
-print("PROGRAM PRI NEZNAMEJ a")
+    t1 = float(input("Vstupná teplota: "))
+    t2 = float(input("Výstupná teplota:  "))
+    ΔL = float(input("Zmena dlžky o: "))
+    delta_t = t2-t1
+    lO = -ΔL
+    a = f"{ΔL/delta_t * lO:.4f}"
+    print("a = ", a)
+    print(" ")
 
-t1 = float(input("Vstupná teplota: "))
-t2 = float(input("Výstupná teplota:  "))
-ΔL = float(input("Zmena dlžky o: "))
-delta_t = t2-t1
-lO = -ΔL
-a = f"{ΔL/delta_t * lO:.4f}"
-print("a = ", a)
-print(" ")
+if ΔL == True:
+    ### Program pri neznamej ΔL
+    print("PROGRAM PRI NEZNAMEJ ΔL ")
 
-
-### Program pri neznamej ΔL
-print("PROGRAM PRI NEZNAMEJ ΔL ")
-
-lo = float(input("Zadaj rozmer: "))
-T1 = float(input("Vstupná teplota: "))
-T2 = float(input("Výstupna teplota:  "))
-a = float(input("koeficient a : "))
-delta_T = T2-T1
-delta_l = f"{lo}*{a}*{delta_T} ={lo*a*delta_T:.4f} "
-print("ΔT = ", delta_T)
-print("ΔL = ", delta_l)
-print("  ")
-print("  ")
-print("  ")
+    lo = float(input("Zadaj rozmer: "))
+    T1 = float(input("Vstupná teplota: "))
+    T2 = float(input("Výstupna teplota:  "))
+    a = float(input("koeficient a : "))
+    delta_T = T2-T1
+    delta_l = f"{lo} * {a} * {delta_T} = {lo * a * delta_T:.4f} "
+    print("ΔT = ", delta_T)
+    print(f'ΔL =  {lo * a * delta_T:.4f}')
+    print("  ")
+    print("  ")
+    print("  ")
